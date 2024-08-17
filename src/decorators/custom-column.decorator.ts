7ad979787cf6c382b535b7bb144ca2c4cbb32237
CaoMeiYouRen <996881204@qq.com>
2024-08-17 20:08:07 +0800

    fix: 新增 CustomColumn 装饰器

---
+++ src/decorators/custom-column.decorator.ts
@@ -0,0 +1,59 @@
+import { applyDecorators } from '@nestjs/common'
+import { Column, ColumnOptions, Index } from 'typeorm'
+import { Length } from 'class-validator'
+import { DATABASE_TYPE } from '@/app.config'
+
+export function CustomColumn(options: ColumnOptions & { index?: boolean }) {
+    const decorators: PropertyDecorator[] = []
+    // if (DATABASE_TYPE === 'sqlite') { // sqlite 的话不修改配置
+
+    // } else
+    let length = options.length
+    if (DATABASE_TYPE === 'mysql') { // 处理 MySQL 不兼容的配置
+        // mysql 索引最大不超过 3072 字节，在 utf8 编码下不超过 1024 字符
+        if (options.index && Number(options.length) > 1024) {
+            options.length = 1024
+        }
+        // mysql 不支持在 text 类型字段上设置 length
+        if (['text', 'mediumtext', 'longtext', 'simple-json', 'simple-array'].includes(options.type as string) && options.length) {
+            if (['text', 'mediumtext', 'longtext'].includes(options.type as string)) {
+                if (Number(options.length) > 16777215) { // 超过 MEDIUMTEXT 的范围
+                    options.type = 'longtext'
+                } else if (Number(options.length) > 65535) { // 超过 TEXT 的范围
+                    options.type = 'mediumtext'
+                } else {
+                    options.type = 'text'
+                }
+            }
+            delete options.length
+        }
+        // mysql 不支持在 simple-json 类型字段上设置 default
+        if (['simple-json'].includes(options.type as string) && options.default) {
+            delete options.default
+        }
+    } else if (DATABASE_TYPE === 'postgres') { // 处理 PostgreSQL 不兼容的配置
+        // postgres 索引最大不超过 8191 字节，在 utf8 编码下不超过 2730 字符
+        if (options.index && Number(options.length) > 2730) {
+            options.length = 2730
+        }
+        // postgres 不支持在 text 类型字段上设置 length
+        // postgres 不支持在 simple-json 类型字段上设置 default
+    }
+    length = options.length || length
+    if (length) {
+        decorators.push(Length(0, Number(length)))
+    }
+
+    decorators.push(Column(options))
+
+    if (options.index) {
+        decorators.push(Index({ unique: options.unique }))// 设置索引
+    }
+
+    // eslint-disable-next-line @typescript-eslint/ban-types
+    return function (target: object | Function, propertyKey?: string | symbol, descriptor?: any) {
+        return applyDecorators(
+            ...decorators,
+        )(target, propertyKey, descriptor) // 手动调用装饰器
+    }
+}
