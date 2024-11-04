01720271b04fb9ac80a05ea4e993b32337313ac1
CaoMeiYouRen <996881204@qq.com>
2024-11-04 15:57:43 +0800

    chore: 修改 eslint 文件

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,6 +13,11 @@ module.exports = {
     rules: {
         'no-useless-constructor': 0,
         'no-console': 0,
-        '@typescript-eslint/indent': 0
+        '@typescript-eslint/indent': 0,
+        'max-len': [0, { code: 200 }], // 强制行的最大长度
+        'max-lines': [1, { max: 500 }], // 强制文件的最大行数
+        'max-lines-per-function': [0, { max: 120 }], // 强制函数最大行数
+        'max-nested-callbacks': [1, { max: 5 }], // 强制回调函数最大嵌套深度
+        'max-params': [1, { max: 5 }], // 强制函数定义中最大参数个数
     },
 }
