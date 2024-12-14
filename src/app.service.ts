402e00bbf737f3171b0c87f4e9f7e56162ffaa3b
CaoMeiYouRen <996881204@qq.com>
2024-12-15 01:26:53 +0800

    refactor(app): 优化获取版本和 Git 信息的时机 - 实现 `OnApplicationBootstrap` 接口，在应用启动时获取版本号和 Git 信息 - 将版本号、Git hash 和 Git 日期存储为类属性以便后续使用

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,12 +1,28 @@
-import { Injectable } from '@nestjs/common'
+import { Injectable, Logger, OnApplicationBootstrap } from '@nestjs/common'
 import fs from 'fs-extra'
 import { getGitInfo } from './utils/git-info'
 
 @Injectable()
-export class AppService {
-    async getHello() {
+export class AppService implements OnApplicationBootstrap {
+
+    private readonly logger = new Logger(AppService.name)
+
+    private version: string
+    private gitHash: string
+    private gitDate: Date
+
+    constructor() { }
+
+    async onApplicationBootstrap() {
         const { version } = await fs.readJson('package.json')
         const { gitHash, gitDate } = await getGitInfo()
+        this.version = version
+        this.gitHash = gitHash
+        this.gitDate = new Date(gitDate)
+    }
+
+    async getHello() {
+        const { version, gitHash, gitDate } = this
         return {
             statusCode: 200,
             message: 'Hello RSS Impact!',
