faf36ca18f1bc50ab1db91263262ca93f42d8748
CaoMeiYouRen <996881204@qq.com>
2025-02-23 00:37:53 +0800

    fix: 更新 push-all-in-one 依赖至 4.3.0，并优化推送类型映射

--- src/interfaces/push-type.ts
+++ src/interfaces/push-type.ts
@@ -1,22 +1,6 @@
-import { PushType, MetaPushConfig } from 'push-all-in-one'
+import { PushType, MetaPushConfig, PushAllInOne } from 'push-all-in-one'
 
-export const PushTypeMap: Record<PushType, string> = {
-    ServerChanTurbo: 'Server酱·Turbo',
-    ServerChanV3: 'Server 酱³',
-    Dingtalk: '钉钉',
-    CustomEmail: '自定义邮件',
-    WechatRobot: '企业微信群机器人',
-    WechatApp: '企业微信应用推送',
-    PushDeer: 'PushDeer',
-    PushPlus: 'PushPlus',
-    IGot: 'iGot',
-    Qmsg: 'Qmsg',
-    XiZhi: '息知',
-    Telegram: 'Telegram',
-    Discord: 'Discord',
-    OneBot: 'OneBot',
-
-}
+export const PushTypeMap: Record<PushType, string> = Object.fromEntries(Object.entries(PushAllInOne).map(([key, value]) => [key as any, value.namespace]))
 
 export const PushTypeList = Object.entries(PushTypeMap).map(([value, label]) => ({
     label,
