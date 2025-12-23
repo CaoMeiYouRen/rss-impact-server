33185a09388b0bbe23172504a26c5d00ce6fcbfc
CaoMeiYouRen <caomeiyouren@qq.com>
2025-12-24 00:11:42 +0800

    fix: 重构 Atom 和 RSS 函数以增强可读性和错误处理

--- src/views/atom.ts
+++ src/views/atom.ts
@@ -2,6 +2,11 @@ import path from 'path'
 import artTemplate from 'art-template'
 import { Data } from '@/interfaces/data'
 
-const Atom = (data: Data) => artTemplate(path.join(__dirname, '../../templates/atom.art'), data).replace(/(\n[\s|\t]*\r*\n)/g, '\n') // 去除多余换行符
-
+const Atom = (data: Data) => {
+    const result = artTemplate(path.join(__dirname, '../../templates/atom.art'), data)
+    if (!result) {
+        throw new Error('Atom template rendering failed')
+    }
+    return result.replace(/(\n[\s|\t]*\r*\n)/g, '\n')
+}
 export default Atom
--- src/views/rss.ts
+++ src/views/rss.ts
@@ -2,6 +2,11 @@ import path from 'path'
 import artTemplate from 'art-template'
 import { Data } from '@/interfaces/data'
 
-const RSS = (data: Data) => artTemplate(path.join(__dirname, '../../templates/rss.art'), data).replace(/(\n[\s|\t]*\r*\n)/g, '\n') // 去除多余换行符
-
+const RSS = (data: Data) => {
+    const result = artTemplate(path.join(__dirname, '../../templates/rss.art'), data)
+    if (!result) {
+        throw new Error('RSS template rendering failed')
+    }
+    return result.replace(/(\n[\s|\t]*\r*\n)/g, '\n')
+}
 export default RSS
